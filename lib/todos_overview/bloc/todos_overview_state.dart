cd4db833b6e441a1a775859903349d570da887f8
hani <hanisoft2022@gmail.com>
2025-07-09 23:04:35 +0900

    todos overview state ongoing..

--- lib/todos_overview/bloc/todos_overview_state.dart
+++ lib/todos_overview/bloc/todos_overview_state.dart
@@ -3,17 +3,17 @@ part of 'todos_overview_bloc.dart';
 enum TodosOverviewStatus { initial, loading, success, failure }
 
 final class TodosOverviewState extends Equatable {
+  final TodosOverviewStatus status;
+  final List<Todo> todos;
+  final TodosViewFilter filter;
+  final Todo? lastDeletedTodo;
+
   const TodosOverviewState({
     this.status = TodosOverviewStatus.initial,
     this.todos = const [],
     this.filter = TodosViewFilter.all,
     this.lastDeletedTodo,
   });
-
-  final TodosOverviewStatus status;
-  final List<Todo> todos;
-  final TodosViewFilter filter;
-  final Todo? lastDeletedTodo;
 
   Iterable<Todo> get filteredTodos => filter.applyAll(todos);
 
