c911a917b29a12e34daad8c6380413ab61ec8f2c
hani <hanisoft2022@gmail.com>
2025-07-11 23:04:31 +0900

    code no error

--- lib/todos_overview/view/todos_overview_page.dart
+++ lib/todos_overview/view/todos_overview_page.dart
@@ -73,17 +73,48 @@ class TodosOverviewView extends StatelessWidget {
         child: BlocBuilder<TodosOverviewBloc, TodosOverviewState>(
           builder: (context, state) {
             if (state.todos.isEmpty) {
-              if (state.status == TodosOverviewStatus.loading) {
-                return const Center(child: CupertinoActivityIndicator());
-              } else if (state.status != TodosOverviewStatus.success) {
-                return const SizedBox();
-              } else {
-                return Center(
-                  child: Text(
-                    l10n.todosOverviewEmptyText,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                );
+              // * 기존 코드
+              // if (state.status == TodosOverviewStatus.loading) {
+              //   return const Center(child: CupertinoActivityIndicator());
+              // } else if (state.status != TodosOverviewStatus.success) {
+              //   return const SizedBox();
+              // } else {
+              //   return Center(
+              //     child: Text(
+              //       l10n.todosOverviewEmptyText,
+              //       style: Theme.of(context).textTheme.bodySmall,
+              //     ),
+              //   );
+              // }
+
+              // * switch문으로 리팩토링한 코드
+              switch (state.status) {
+                case TodosOverviewStatus.initial:
+                  // 앱이 시작되고 아직 아무런 데이터도 로딩되지 않은 상태
+                  // 보통은 아무것도 보여주지 않거나, Splash/Placeholder 등을 보여줄 수 있습니다.
+                  return const SizedBox();
+
+                case TodosOverviewStatus.loading:
+                  // 데이터 로딩 중
+                  return const Center(child: CupertinoActivityIndicator());
+
+                case TodosOverviewStatus.success:
+                  // 데이터는 성공적으로 불러왔으나, 할 일이 없음
+                  return Center(
+                    child: Text(
+                      l10n.todosOverviewEmptyText,
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  );
+
+                case TodosOverviewStatus.failure:
+                  // 데이터 로딩 실패
+                  return Center(
+                    child: Text(
+                      l10n.todosOverviewErrorSnackbarText,
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(color: Colors.red),
+                    ),
+                  );
               }
             }
 
