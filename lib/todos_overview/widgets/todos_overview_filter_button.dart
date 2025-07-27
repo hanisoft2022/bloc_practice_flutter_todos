d6136450408986465092c53403a012f980a4d9da
hani <hanisoft2022@gmail.com>
2025-07-27 10:27:43 +0900

    refactor: context.select to BlocSelector

--- lib/todos_overview/widgets/todos_overview_filter_button.dart
+++ lib/todos_overview/widgets/todos_overview_filter_button.dart
@@ -10,38 +10,34 @@ class TodosOverviewFilterButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
 
-    final activeFilter = context.select(
-      (TodosOverviewBloc bloc) => bloc.state.filter,
-    );
-
-    return PopupMenuButton<TodosViewFilter>(
-      shape: const ContinuousRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
-      initialValue: activeFilter,
-      tooltip: l10n.todosOverviewFilterTooltip,
-      onSelected: (filter) {
-        context.read<TodosOverviewBloc>().add(
-          TodosOverviewFilterChanged(filter),
+    return BlocSelector<TodosOverviewBloc, TodosOverviewState, TodosViewFilter>(
+      selector: (state) => state.filter,
+      builder: (context, activeFilter) {
+        return PopupMenuButton<TodosViewFilter>(
+          shape: const ContinuousRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          initialValue: activeFilter,
+          tooltip: l10n.todosOverviewFilterTooltip,
+          onSelected: (filter) {
+            context.read<TodosOverviewBloc>().add(TodosOverviewFilterChanged(filter));
+          },
+          itemBuilder: (context) {
+            return [
+              PopupMenuItem(value: TodosViewFilter.all, child: Text(l10n.todosOverviewFilterAll)),
+              PopupMenuItem(
+                value: TodosViewFilter.activeOnly,
+                child: Text(l10n.todosOverviewFilterActiveOnly),
+              ),
+              PopupMenuItem(
+                value: TodosViewFilter.completedOnly,
+                child: Text(l10n.todosOverviewFilterCompletedOnly),
+              ),
+            ];
+          },
+          icon: const Icon(Icons.filter_list_rounded),
         );
       },
-      itemBuilder: (context) {
-        return [
-          PopupMenuItem(
-            value: TodosViewFilter.all,
-            child: Text(l10n.todosOverviewFilterAll),
-          ),
-          PopupMenuItem(
-            value: TodosViewFilter.activeOnly,
-            child: Text(l10n.todosOverviewFilterActiveOnly),
-          ),
-          PopupMenuItem(
-            value: TodosViewFilter.completedOnly,
-            child: Text(l10n.todosOverviewFilterCompletedOnly),
-          ),
-        ];
-      },
-      icon: const Icon(Icons.filter_list_rounded),
     );
   }
 }
